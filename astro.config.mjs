7b4ebf05127822d5a5e7178670484baacdf197fd
WEI HAN CHEN <weihanchen0725@gmail.com>
2023-09-10 16:36:42 -0700

    Config astro.config.mjs to match with github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://weihanchen0725.github.io',
   integrations: [solidJs()]
 });
